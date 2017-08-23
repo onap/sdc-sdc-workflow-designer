bdaffdf812fa909ebb37180451b68cde9c260d0a
Lvbo163 <lv.bo163@zte.com.cn>
2017-08-23 17:27:33 +0800

    upload init code and doc for workflow designer
    Issue-ID: SDC-240
    
    Change-Id: I79507427bffa3938d7f312aaf1a29f238a87cde1
    Signed-off-by: Lvbo163 <lv.bo163@zte.com.cn>

---
+++ sdc-workflow-designer-ui/src/app/app.component.ts
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2017 ZTE Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     ZTE - initial API and implementation and/or initial documentation
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'SDC Workflow Designer';
+}
