78f074e7aaa6daa5af34089b19c70d11fdc758d9
Lvbo163 <lv.bo163@zte.com.cn>
2017-09-27 17:05:21 +0800

    add unit test for bpmn workflow template convert
    Issue-ID: SDC-409
    
    Change-Id: I7a7b09c9d9f07b580a9bd6be158bdf606a77717f
    Signed-off-by: Lvbo163 <lv.bo163@zte.com.cn>

---
+++ sdc-workflow-designer-server/src/test/java/org/onap/sdc/workflowdesigner/planwriter/BpmnPlanArtefactWriterTest.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2017 ZTE Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the Apache License, Version 2.0
+ * and the Eclipse Public License v1.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     ZTE - initial API and implementation and/or initial documentation
+ */
+package org.onap.sdc.workflowdesigner.planwriter;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.onap.sdc.workflowdesigner.model.Process;
+import org.onap.sdc.workflowdesigner.writer.BpmnPlanArtefactWriter;
+
+public class BpmnPlanArtefactWriterTest {
+
+    @Test
+    public void testWritePlan() throws Exception {
+        BpmnPlanArtefactWriter writer = new BpmnPlanArtefactWriter(mockProcss());
+        String result = writer.completePlanTemplate();
+        assertEquals(result, getResult());
+    }
+
+    private Process mockProcss() {
+        Process process = new Process("templateTest");
+
+        return process;
+    }
+
+    public String getResult() throws IOException {
+        StringBuffer buffer = new StringBuffer();
+
+        String path = "src/test/resources/workflow/template-test.bpmn20.xml";
+        BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
+
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line).append("\r\n");
+        }
+
+        if (reader != null) {
+            reader.close();
+        }
+
+        return buffer.toString();
+    }
+
+}
