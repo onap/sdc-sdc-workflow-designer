e1c5e19016027b0e10124aefcc6954d3a020361e
Lvbo163 <lv.bo163@zte.com.cn>
2017-09-27 09:45:44 +0800

    Add unit test for config
    Issue-ID: SDC-403
    
    Change-Id: I5acdc670e274631225608899f24349bd037e5067
    Signed-off-by: Lvbo163 <lv.bo163@zte.com.cn>

---
+++ sdc-workflow-designer-server/src/test/java/org/onap/sdc/workflowdesigner/config/ConfigTest.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2017 ZTE Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and the Apache License 2.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     ZTE - initial API and implementation and/or initial documentation
+ */
+package org.onap.sdc.workflowdesigner.config;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.junit.Test;
+
+public class ConfigTest {
+    @Test
+    public void testLoad() throws ResourceNotFoundException, ParseErrorException, Exception {
+        assertEquals(Config.PROPERTIES.get(Config.HANDLER_ClASS),
+                "com.zte.ums.workflow.activitiext.restservicetask.HttpUtil");
+    }
+}
