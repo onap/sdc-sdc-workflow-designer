d774a0ce19fc6abb2ce8af82d5d7ca811b947c54
YuanHu <yuan.hu1@zte.com.cn>
2018-03-07 17:13:17 +0800

    Fixed the bugs checked by sonar.
    Add null pointer detected and protected.
    
    Issue-ID: SDC-1079
    
    Change-Id: I39c5280746d8ca2b476cbe27520d34370cb9075b
    Signed-off-by: YuanHu <yuan.hu1@zte.com.cn>

--- sdc-workflow-designer-server/src/main/java/org/onap/sdc/workflowdesigner/parser/Bpmn4ToscaJsonParser.java
+++ sdc-workflow-designer-server/src/main/java/org/onap/sdc/workflowdesigner/parser/Bpmn4ToscaJsonParser.java
@@ -47,180 +47,188 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class Bpmn4ToscaJsonParser {
 
-    private static Logger log = LoggerFactory.getLogger(Bpmn4ToscaJsonParser.class);
+  private static Logger log = LoggerFactory.getLogger(Bpmn4ToscaJsonParser.class);
 
-    private static ObjectMapper MAPPER = new ObjectMapper();
-    
-    private Map<String, JsonNode> restConfigMap = new HashMap<String, JsonNode>();
+  private static ObjectMapper MAPPER = new ObjectMapper();
 
-    static {
-        MAPPER.enable(SerializationFeature.INDENT_OUTPUT);
-        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  private Map<String, JsonNode> restConfigMap = new HashMap<String, JsonNode>();
+
+  static {
+    MAPPER.enable(SerializationFeature.INDENT_OUTPUT);
+    MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  }
+
+  public Process parse(String processName, URI jsonFileUrl)
+      throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
+    Process process = new Process(processName);
+
+    JsonNode rootNode = MAPPER.readValue(jsonFileUrl.toURL(), JsonNode.class);
+
+    log.debug("Creating Process models...");
+    JsonNode nodes = rootNode.get(JsonKeys.NODES);
+    if (nodes == null) {
+      return process;
     }
 
-    public Process parse(String processName, URI jsonFileUrl)
-            throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
-        Process process = new Process(processName);
+    this.loadConfigs(rootNode.get(JsonKeys.CONFIGS));
 
-        JsonNode rootNode = MAPPER.readValue(jsonFileUrl.toURL(), JsonNode.class);
+    Iterator<JsonNode> iter = nodes.iterator();
+    while (iter.hasNext()) {
+      JsonNode jsonNode = (JsonNode) iter.next();
 
-        log.debug("Creating Process models...");
-        JsonNode nodes = rootNode.get(JsonKeys.NODES);
-        if (nodes == null) {
-            return process;
-        }
-        
-        this.loadConfigs(rootNode.get(JsonKeys.CONFIGS));
+      // get element
+      Element element = createElementFromJson(jsonNode);
+      process.getElementList().add(element);
 
-        Iterator<JsonNode> iter = nodes.iterator();
-        while (iter.hasNext()) {
-            JsonNode jsonNode = (JsonNode) iter.next();
+      // get sequence flows
+      List<SequenceFlow> flowList = getSequenceFlows(jsonNode);
+      process.getSequenceFlowList().addAll(flowList);
 
-            // get element
-            Element element = createElementFromJson(jsonNode);
-            process.getElementList().add(element);
-
-            // get sequence flows
-            List<SequenceFlow> flowList = getSequenceFlows(jsonNode);
-            process.getSequenceFlowList().addAll(flowList);
-
-            // add dataObject
-            if (element instanceof StartEvent) {
-                List<DataObject> dataObjects = this.getDataObject((StartEvent) element);
-                process.getDataObjectList().addAll(dataObjects);
-            }
-        }
-
-        return process;
-
+      // add dataObject
+      if (element instanceof StartEvent) {
+        List<DataObject> dataObjects = this.getDataObject((StartEvent) element);
+        process.getDataObjectList().addAll(dataObjects);
+      }
     }
 
-    private List<DataObject> getDataObject(StartEvent startEvent) {
-        List<DataObject> dataObjects = new ArrayList<DataObject>();
+    return process;
 
-        for (Parameter parameter : startEvent.getParameters()) {
-            DataObject dataObject = new DataObject();
-            dataObject.setId(parameter.getName());
-            dataObject.setName(parameter.getName());
-            dataObject.setValue((String) parameter.getValue());
+  }
 
-            dataObjects.add(dataObject);
-        }
+  private List<DataObject> getDataObject(StartEvent startEvent) {
+    List<DataObject> dataObjects = new ArrayList<DataObject>();
 
-        return dataObjects;
-    }
-    
-    private void loadConfigs(JsonNode config) {
-        if(config == null) {
-            return;
-        }
-        loadRestConfigs(config.get(JsonKeys.REST_CONFIGS));
-    }
-    
-    private void loadRestConfigs(JsonNode restConfigs) {
-        if(restConfigs == null) {
-            return;
-        }
-        
-        Iterator<JsonNode> iter = restConfigs.iterator();
-        while (iter.hasNext()) {
-            JsonNode restConfig = (JsonNode) iter.next();
+    for (Parameter parameter : startEvent.getParameters()) {
+      DataObject dataObject = new DataObject();
+      dataObject.setId(parameter.getName());
+      dataObject.setName(parameter.getName());
+      dataObject.setValue((String) parameter.getValue());
 
-            String configId = getValueFromJsonNode(restConfig, JsonKeys.ID); 
-            restConfigMap.put(configId, restConfig);
-        }
+      dataObjects.add(dataObject);
     }
 
-    private List<SequenceFlow> getSequenceFlows(JsonNode jsonNode) {
-        List<SequenceFlow> flowList = new ArrayList<SequenceFlow>();
-        JsonNode sequenceFlowNodes = jsonNode.get(JsonKeys.SEQUENCE_FLOWS);
+    return dataObjects;
+  }
 
-        Iterator<JsonNode> iter = sequenceFlowNodes.iterator();
-        while (iter.hasNext()) {
-            JsonNode connectionEntry = (JsonNode) iter.next();
-            String sourceRef = getValueFromJsonNode(connectionEntry, JsonKeys.SOURCE_REF);
-            String targetRef = getValueFromJsonNode(connectionEntry, JsonKeys.TARGET_REF);
-            String condition = getValueFromJsonNode(connectionEntry, JsonKeys.CONDITION);
-            SequenceFlow flow = new SequenceFlow();
-            flow.setId(sourceRef + targetRef);
-            flow.setSourceRef(sourceRef);
-            flow.setTargetRef(targetRef);
-            flow.setCondition(condition);
-            flowList.add(flow);
-        }
+  private void loadConfigs(JsonNode config) {
+    if (config == null) {
+      return;
+    }
+    loadRestConfigs(config.get(JsonKeys.REST_CONFIGS));
+  }
 
-        return flowList;
+  private void loadRestConfigs(JsonNode restConfigs) {
+    if (restConfigs == null) {
+      return;
     }
 
-    protected Element createElementFromJson(JsonNode jsonNode)
-            throws JsonParseException, JsonMappingException, IOException {
-        String jsonObject = jsonNode.toString();
-        Element element;
+    Iterator<JsonNode> iter = restConfigs.iterator();
+    while (iter.hasNext()) {
+      JsonNode restConfig = (JsonNode) iter.next();
 
-        String nodeType = getValueFromJsonNode(jsonNode, JsonKeys.TYPE);
-        switch (nodeType) {
-        case "startEvent":
-            element = MAPPER.readValue(jsonObject, StartEvent.class);
-            break;
-        case "endEvent":
-            element = MAPPER.readValue(jsonObject, EndEvent.class);
-            break;
-        case "errorStartEvent":
-            element = MAPPER.readValue(jsonObject, ErrorStartEvent.class);
-            break;
-        case "errorEndEvent":
-            element = MAPPER.readValue(jsonObject, ErrorEndEvent.class);
-            break;
-        case "intermediateCatchEvent":
-            element = MAPPER.readValue(jsonObject, IntermediateCatchEvent.class);
-            break;
-        case "serviceTask":
-            element = MAPPER.readValue(jsonObject, ServiceTask.class);
-            break;
-        case "restTask":
-            element = this.createRestServiceTask(jsonObject);
-            break;
-        case "scriptTask":
-            element = MAPPER.readValue(jsonObject, ScriptTask.class);
-            break;
-        case "exclusiveGateway":
-            element = MAPPER.readValue(jsonObject, ExclusiveGateway.class);
-            break;
-        case "parallelGateway":
-            element = MAPPER.readValue(jsonObject, ParallelGateway.class);
-            break;
-        default:
-            log.warn("Ignoring node: type '" + nodeType + "' is unkown");
-            return null;
-        }
-
-        return element;
+      String configId = getValueFromJsonNode(restConfig, JsonKeys.ID);
+      restConfigMap.put(configId, restConfig);
     }
-    
-    private RestServiceTask createRestServiceTask(String jsonObject) throws JsonParseException, JsonMappingException, IOException {
-        RestServiceTask restServiceTask = MAPPER.readValue(jsonObject, RestServiceTask.class);
-        
-        // add baseUrl to relative url
-        String restConfigId = restServiceTask.getRestConfigId();
-        JsonNode restConfig = this.restConfigMap.get(restConfigId);
-        
-        if(restConfig != null) { // while create a new rest task and didnot set method, the restconfig info may be null
-            restServiceTask.setUrl(getValueFromJsonNode(restConfig, JsonKeys.MICROSERVICE_URL));
-            restServiceTask.setServiceName(getValueFromJsonNode(restConfig, JsonKeys.MICROSERVICE_NAME));
-            restServiceTask.setServiceVersion(getValueFromJsonNode(restConfig, JsonKeys.MICROSERVICE_VERSION));
-        }
-        
-        for(Parameter parameter : restServiceTask.getParameters()) {
-            if("body".equals(parameter.getPosition())) {
-                parameter.setValueSource(null);
-            }
-        }
-        
-        return restServiceTask;
+  }
+
+  private List<SequenceFlow> getSequenceFlows(JsonNode jsonNode) {
+    List<SequenceFlow> flowList = new ArrayList<SequenceFlow>();
+    JsonNode sequenceFlowNodes = jsonNode.get(JsonKeys.SEQUENCE_FLOWS);
+
+    Iterator<JsonNode> iter = sequenceFlowNodes.iterator();
+    while (iter.hasNext()) {
+      JsonNode connectionEntry = (JsonNode) iter.next();
+      String sourceRef = getValueFromJsonNode(connectionEntry, JsonKeys.SOURCE_REF);
+      String targetRef = getValueFromJsonNode(connectionEntry, JsonKeys.TARGET_REF);
+      String condition = getValueFromJsonNode(connectionEntry, JsonKeys.CONDITION);
+      SequenceFlow flow = new SequenceFlow();
+      flow.setId(sourceRef + targetRef);
+      flow.setSourceRef(sourceRef);
+      flow.setTargetRef(targetRef);
+      flow.setCondition(condition);
+      flowList.add(flow);
     }
 
-    private String getValueFromJsonNode(JsonNode jsonNode, String key) {
-        return jsonNode.get(key) == null ? null : jsonNode.get(key).asText();
+    return flowList;
+  }
+
+  protected Element createElementFromJson(JsonNode jsonNode)
+      throws JsonParseException, JsonMappingException, IOException {
+    String jsonObject = jsonNode.toString();
+    Element element;
+
+    String nodeType = getValueFromJsonNode(jsonNode, JsonKeys.TYPE);
+    if (nodeType == null) {
+      log.warn("Ignoring node: type is null");
+      return null;
     }
+
+    switch (nodeType) {
+      case "startEvent":
+        element = MAPPER.readValue(jsonObject, StartEvent.class);
+        break;
+      case "endEvent":
+        element = MAPPER.readValue(jsonObject, EndEvent.class);
+        break;
+      case "errorStartEvent":
+        element = MAPPER.readValue(jsonObject, ErrorStartEvent.class);
+        break;
+      case "errorEndEvent":
+        element = MAPPER.readValue(jsonObject, ErrorEndEvent.class);
+        break;
+      case "intermediateCatchEvent":
+        element = MAPPER.readValue(jsonObject, IntermediateCatchEvent.class);
+        break;
+      case "serviceTask":
+        element = MAPPER.readValue(jsonObject, ServiceTask.class);
+        break;
+      case "restTask":
+        element = this.createRestServiceTask(jsonObject);
+        break;
+      case "scriptTask":
+        element = MAPPER.readValue(jsonObject, ScriptTask.class);
+        break;
+      case "exclusiveGateway":
+        element = MAPPER.readValue(jsonObject, ExclusiveGateway.class);
+        break;
+      case "parallelGateway":
+        element = MAPPER.readValue(jsonObject, ParallelGateway.class);
+        break;
+      default:
+        log.warn("Ignoring node: type '" + nodeType + "' is unkown");
+        return null;
+    }
+
+    return element;
+  }
+
+  private RestServiceTask createRestServiceTask(String jsonObject)
+      throws JsonParseException, JsonMappingException, IOException {
+    RestServiceTask restServiceTask = MAPPER.readValue(jsonObject, RestServiceTask.class);
+
+    // add baseUrl to relative url
+    String restConfigId = restServiceTask.getRestConfigId();
+    JsonNode restConfig = this.restConfigMap.get(restConfigId);
+
+    if (restConfig != null) { // while create a new rest task and didnot set method, the restconfig
+                              // info may be null
+      restServiceTask.setUrl(getValueFromJsonNode(restConfig, JsonKeys.MICROSERVICE_URL));
+      restServiceTask.setServiceName(getValueFromJsonNode(restConfig, JsonKeys.MICROSERVICE_NAME));
+      restServiceTask
+          .setServiceVersion(getValueFromJsonNode(restConfig, JsonKeys.MICROSERVICE_VERSION));
+    }
+
+    for (Parameter parameter : restServiceTask.getParameters()) {
+      if ("body".equals(parameter.getPosition())) {
+        parameter.setValueSource(null);
+      }
+    }
+
+    return restServiceTask;
+  }
+
+  private String getValueFromJsonNode(JsonNode jsonNode, String key) {
+    return jsonNode.get(key) == null ? null : jsonNode.get(key).asText();
+  }
 
 }
