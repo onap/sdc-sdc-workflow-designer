249d5131d866d2a20a7903b068438527f17c46e1
YuanHu <yuan.hu1@zte.com.cn>
2018-03-12 09:22:52 +0800

    Add Unit Test code.
    Add Unit Test code.
    
    Issue-ID: SDC-1078
    
    Change-Id: I5166090d0f819fc8c97dfb19e1d095f5d1a92364
    Signed-off-by: YuanHu <yuan.hu1@zte.com.cn>

---
+++ sdc-workflow-designer-server/src/test/java/org/onap/sdc/workflowdesigner/model/ElementTest.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2018 ZTE Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the Apache License, Version 2.0
+ * and the Eclipse Public License v1.0 which both accompany this distribution,
+ * and are available at http://www.eclipse.org/legal/epl-v10.html
+ * and http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Contributors:
+ *     ZTE - initial API and implementation and/or initial documentation
+ */
+
+package org.onap.sdc.workflowdesigner.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class ElementTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {}
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @After
+  public void tearDown() throws Exception {}
+
+  /**
+   * Test method for {@link org.onap.sdc.workflowdesigner.model.Element#getType()}
+   * 
+   */
+  @Test
+  public final void getType() {
+    Element endEvent = new EndEvent();
+    System.out.println(endEvent.getType());
+    assertEquals(endEvent.getType(), null);
+  }
+
+}
