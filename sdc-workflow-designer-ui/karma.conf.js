66266de80df6d7a87add8ef135bf9ada63946739
YuanHu <yuan.hu1@zte.com.cn>
2018-03-28 11:26:36 +0800

    Modify the year of licenses
    Modify the year of licenses
    
    Issue-ID: SDC-1079
    
    Change-Id: I73c9b8497892ee2ccc896ad2b3dc7b707bda5138
    Signed-off-by: YuanHu <yuan.hu1@zte.com.cn>

--- sdc-workflow-designer-ui/karma.conf.js
+++ sdc-workflow-designer-ui/karma.conf.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 ZTE Corporation.
+ * Copyright (c) 2017-2018 ZTE Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and the Apache License 2.0 which both accompany this distribution,
