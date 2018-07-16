7e78cd19bbb2b0730fafc0b328a71c69f5730e16
priyanshu <pagarwal@amdocs.com>
2018-07-16 19:25:05 +0530

    Workflow Docker containers
    1. Implemented workflow init container.
    2. Implemented workflow backend container.
    3. Added a placeholder for frontend module container.
    4. Modifiled directory structure and POM files.
    5. Added a ReadMe with sample commands to detail how to run the containers.
    
    Change-Id: Ib110fcc104ce72ff28fce514a1475a032cc2d82b
    Issue-ID: SDC-1482
    Signed-off-by: priyanshu <pagarwal@amdocs.com>
    Signed-off-by: vempo <vitaliy.emporopulo@amdocs.com>
    Signed-off-by: priyanshu <pagarwal@amdocs.com>

---
+++ workflow/workflow-designer-be/src/test/java/org/onap/sdc/workflow/services/impl/mappers/VersionStateMapperTest.java
@@ -0,0 +1,75 @@
+package org.onap.sdc.workflow.services.impl.mappers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.onap.sdc.workflow.persistence.types.WorkflowVersionState;
+import org.openecomp.sdc.versioning.dao.types.VersionStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(classes = VersionStateMapperTest.VersionStatusMapperSpringTestConfig.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class VersionStateMapperTest {
+
+    @Configuration
+    @ComponentScan(basePackageClasses = {VersionStateMapper.class})
+    public static class VersionStatusMapperSpringTestConfig { }
+
+
+    @Autowired
+    VersionStateMapper versionStateMapper;
+
+    @Test
+    public void shouldMapCertifiedVersionStatusToWorkflowVersionStatus() {
+        WorkflowVersionState mappedVersionStatus =
+                versionStateMapper.versionStatusToWorkflowVersionState(VersionStatus.Certified);
+        assertEquals(WorkflowVersionState.CERTIFIED, mappedVersionStatus);
+    }
+
+    @Test
+    public void shouldMapDraftVersionStatusToWorkflowVersionStatus() {
+        WorkflowVersionState mappedVersionStatus =
+                versionStateMapper.versionStatusToWorkflowVersionState(VersionStatus.Draft);
+        assertEquals(WorkflowVersionState.DRAFT, mappedVersionStatus);
+    }
+
+    @Test
+    public void shouldMapDeletedVersionStatusToWorkflowVersionStatus() {
+        WorkflowVersionState mappedVersionStatus =
+                versionStateMapper.versionStatusToWorkflowVersionState(VersionStatus.Deleted);
+        assertEquals(WorkflowVersionState.DRAFT, mappedVersionStatus);
+    }
+
+    @Test
+    public void shouldMapLockedVersionStatusToWorkflowVersionStatus() {
+        WorkflowVersionState mappedVersionStatus =
+                versionStateMapper.versionStatusToWorkflowVersionState(VersionStatus.Locked);
+        assertEquals(WorkflowVersionState.DRAFT, mappedVersionStatus);
+    }
+
+    @Test
+    public void shouldMapDeprecatedVersionStatusToWorkflowVersionStatus() {
+        WorkflowVersionState mappedVersionStatus =
+                versionStateMapper.versionStatusToWorkflowVersionState(VersionStatus.Deprecated);
+        assertEquals(WorkflowVersionState.DRAFT, mappedVersionStatus);
+    }
+
+    @Test
+    public void shouldMapCertifiedWorkflowVersionStatusToVersionStatus() {
+        VersionStatus mappedVersionStatus =
+                versionStateMapper.workflowVersionStateToVersionStatus(WorkflowVersionState.CERTIFIED);
+        assertEquals(VersionStatus.Certified, mappedVersionStatus);
+    }
+
+    @Test
+    public void shouldMapDraftWorkflowVersionStatusToVersionStatus() {
+        VersionStatus mappedVersionStatus =
+                versionStateMapper.workflowVersionStateToVersionStatus(WorkflowVersionState.DRAFT);
+        assertEquals(VersionStatus.Draft, mappedVersionStatus);
+    }
+}
