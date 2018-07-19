ab219fc3d51a30479acc750ace3552d08c3d4e2c
avigaffa <avi.gaffa@amdocs.com>
2018-07-19 13:58:06 +0300

    Add Logging support for WF
    Reverted changes of application.properties
    Added logging supprt for workflow application
    
    Change-Id: Ib0d0ff8d159168a752edca4f80b6a9b5810d52b7
    Issue-ID: SDC-1530
    Signed-off-by: avigaffa <avi.gaffa@amdocs.com>

---
+++ workflow/workflow-designer-be/src/main/java/org/onap/sdc/workflow/logging/config/InterceptorConfig.java
@@ -0,0 +1,19 @@
+package org.onap.sdc.workflow.logging.config;
+
+import org.onap.sdc.workflow.logging.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Autowired
+    LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
