c7134ada058da36a6cdd6d1bc8f74459ca4bc83b
ayalaben <ayala.benzvi@amdocs.com>
2018-07-29 14:46:38 +0300

    Publish version exception handling
    Change-Id: If4e172f40c7410c5fa90d5686733f4d995b98d17
    Issue-ID: SDC-1518
    Signed-off-by: ayalaben <ayala.benzvi@amdocs.com>

--- workflow-designer-be/src/main/java/org/onap/sdc/workflow/services/impl/WorkflowVersionManagerImpl.java
+++ workflow-designer-be/src/main/java/org/onap/sdc/workflow/services/impl/WorkflowVersionManagerImpl.java
@@ -16,6 +16,7 @@
 
 package org.onap.sdc.workflow.services.impl;
 
+import static com.amdocs.zusammen.datatypes.response.ErrorCode.ZU_ITEM_VERSION_PUBLISH;
 import static org.onap.sdc.workflow.persistence.types.WorkflowVersionState.CERTIFIED;
 
 import java.io.IOException;
@@ -43,6 +44,7 @@ import org.onap.sdc.workflow.services.exceptions.VersionModificationException;
 import org.onap.sdc.workflow.services.exceptions.VersionStateModificationException;
 import org.onap.sdc.workflow.services.impl.mappers.VersionMapper;
 import org.onap.sdc.workflow.services.impl.mappers.VersionStateMapper;
+import org.openecomp.sdc.common.errors.SdcRuntimeException;
 import org.openecomp.sdc.logging.api.Logger;
 import org.openecomp.sdc.logging.api.LoggerFactory;
 import org.openecomp.sdc.versioning.VersioningManager;
@@ -95,6 +97,7 @@ public class WorkflowVersionManagerImpl implements WorkflowVersionManager {
     public WorkflowVersion get(String workflowId, String versionId) {
         WorkflowVersion workflowVersion = versionMapper.versionToWorkflowVersion(getVersion(workflowId, versionId));
         loadAndAddParameters(workflowId, workflowVersion);
+        workflowVersion.setHasArtifact(artifactRepository.get(workflowId,versionId).isPresent());
         return workflowVersion;
     }
 
@@ -140,7 +143,18 @@ public class WorkflowVersionManagerImpl implements WorkflowVersionManager {
         updateParameters(workflowId, version.getId(), workflowVersion.getInputs(), workflowVersion.getOutputs());
 
         versioningManager.updateVersion(workflowId, version);
-        versioningManager.publish(workflowId, version, "Update version");
+        try {
+            versioningManager.publish(workflowId, version, "Update version");
+        } catch (SdcRuntimeException ex) {
+            if (ex.getMessage().contains(String.valueOf(ZU_ITEM_VERSION_PUBLISH))) {
+                //If no changes to publish exception - ignore
+                LOGGER.error(
+                        String.format("Workflow id: %s. Version id: %s. Error: %s", workflowId, workflowVersion.getId(),
+                                ex.getMessage()));
+            } else {
+                throw ex;
+            }
+        }
     }
 
     @Override
