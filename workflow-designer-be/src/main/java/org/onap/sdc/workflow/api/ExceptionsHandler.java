58eac31ce4f32bf1ec594ee24464924fe6031b6a
talig <talig@amdocs.com>
2018-08-14 11:21:46 +0300

    Map unexpected error to response
    Change-Id: I84293aa040b736670885f01d625862cf41e359d2
    Issue-ID: SDC-1647
    Signed-off-by: talig <talig@amdocs.com>

--- workflow-designer-be/src/main/java/org/onap/sdc/workflow/api/ExceptionsHandler.java
+++ workflow-designer-be/src/main/java/org/onap/sdc/workflow/api/ExceptionsHandler.java
@@ -17,12 +17,14 @@
 package org.onap.sdc.workflow.api;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.text.RandomStringGenerator;
 import org.onap.sdc.workflow.api.types.ErrorResponse;
+import org.onap.sdc.workflow.api.types.UnexpectedErrorResponse;
 import org.onap.sdc.workflow.services.exceptions.EntityNotFoundException;
 import org.onap.sdc.workflow.services.exceptions.InvalidArtifactException;
 import org.onap.sdc.workflow.services.exceptions.UniqueValueViolationException;
@@ -30,6 +32,8 @@ import org.onap.sdc.workflow.services.exceptions.VersionCreationException;
 import org.onap.sdc.workflow.services.exceptions.VersionModificationException;
 import org.onap.sdc.workflow.services.exceptions.VersionStateModificationException;
 import org.onap.sdc.workflow.services.exceptions.VersionStatusModificationException;
+import org.openecomp.sdc.logging.api.Logger;
+import org.openecomp.sdc.logging.api.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -46,44 +50,80 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class ExceptionsHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public final ResponseEntity<ErrorResponse> handleNotFoundException(Exception exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), NOT_FOUND);
-    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionsHandler.class);
+    private static final UnexpectedExceptionMapper UNEXPECTED_EXCEPTION_MAPPER =
+            String.valueOf(false).equalsIgnoreCase(System.getProperty("errors.includeDevInfo"))
+                    ? new UnexpectedExceptionMapperImpl()
+                    : new UnexpectedExceptionMapperDevImpl();
 
-    @ExceptionHandler({InvalidArtifactException.class, VersionModificationException.class,
-            VersionStateModificationException.class, VersionStatusModificationException.class,
-            UniqueValueViolationException.class})
-    public final ResponseEntity<ErrorResponse> handleUnprocessableEntityException(Exception exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), UNPROCESSABLE_ENTITY);
-    }
-
-    @ExceptionHandler(VersionCreationException.class)
-    public final ResponseEntity<ErrorResponse> handleVersioningErrorException(VersionCreationException exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), FORBIDDEN);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<ErrorResponse> handleUnexpectedException(Exception exception) {
-        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), INTERNAL_SERVER_ERROR);
-    }
-
-    //For missing header exceptions
     @Override
     public ResponseEntity<Object> handleServletRequestBindingException(ServletRequestBindingException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
+        //For missing header exceptions
+        LOGGER.error(ex.getMessage(), ex);
         return new ResponseEntity<>(new ErrorResponse(ex.getMessage()), BAD_REQUEST);
     }
 
     @Override
     protected final ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException exception,
-            final HttpHeaders headers,
-            final HttpStatus status,
-            final WebRequest request) {
-
+            final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
         String errorMsg = exception.getBindingResult().getFieldErrors().stream()
                                   .map(DefaultMessageSourceResolvable::getDefaultMessage).findFirst()
                                   .orElse(exception.getMessage());
+        LOGGER.error(errorMsg, exception);
         return new ResponseEntity<>(new ErrorResponse(errorMsg), BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public final ResponseEntity<ErrorResponse> handleNotFoundException(Exception exception) {
+        return mapToErrorResponse(exception, NOT_FOUND);
+    }
+
+    @ExceptionHandler(
+            {InvalidArtifactException.class, VersionCreationException.class, VersionModificationException.class,
+                    VersionStateModificationException.class, VersionStatusModificationException.class,
+                    UniqueValueViolationException.class})
+    public final ResponseEntity<ErrorResponse> handleUnprocessableEntityException(Exception exception) {
+        return mapToErrorResponse(exception, UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<UnexpectedErrorResponse> handleUnexpectedException(Exception exception) {
+        UnexpectedErrorResponse response = UNEXPECTED_EXCEPTION_MAPPER.map(exception);
+        LOGGER.error(response.getMessage(), exception);
+        return new ResponseEntity<>(response, INTERNAL_SERVER_ERROR);
+    }
+
+    private static ResponseEntity<ErrorResponse> mapToErrorResponse(Exception exception, HttpStatus httpStatus) {
+        LOGGER.error(exception.getMessage(), exception);
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), httpStatus);
+    }
+
+    private interface UnexpectedExceptionMapper {
+
+        UnexpectedErrorResponse map(Exception exception);
+    }
+
+    private static class UnexpectedExceptionMapperImpl implements UnexpectedExceptionMapper {
+
+        private static final String ERROR_MSG = "Something bad happened. Please contact support with code %s";
+        private static final RandomStringGenerator CODE_GENERATOR =
+                new RandomStringGenerator.Builder().withinRange('A', 'Z').build();
+        private static final int CODE_LENGTH = 8;
+
+        @Override
+        public UnexpectedErrorResponse map(Exception exception) {
+            return new UnexpectedErrorResponse(String.format(ERROR_MSG, CODE_GENERATOR.generate(CODE_LENGTH)));
+        }
+    }
+
+    private static class UnexpectedExceptionMapperDevImpl extends UnexpectedExceptionMapperImpl {
+
+        @Override
+        public UnexpectedErrorResponse map(Exception exception) {
+            UnexpectedErrorResponse response = super.map(exception);
+            response.setDevInfo(ExceptionUtils.getStackTrace(exception));
+            return response;
+        }
     }
 }
