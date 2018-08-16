bd707b6c0dd34e247ea912bf2f1f9ae2a469ea6b
siddharth0905 <siddharth.singh4@amdocs.com>
2018-08-16 11:08:21 +0300

    Add audit logging for WF
    Change-Id: Ia57e7b3c7208df962fcc7305822a055984aafe0b
    Issue-ID: SDC-1530
    Signed-off-by: siddharth0905 <siddharth.singh4@amdocs.com>
    Signed-off-by: vempo <vitaliy.emporopulo@amdocs.com>

---
+++ workflow-designer-be/src/main/java/org/onap/sdc/workflow/server/config/LoggingInterceptorConfig.java
@@ -0,0 +1,23 @@
+package org.onap.sdc.workflow.server.config;
+
+import org.openecomp.sdc.logging.servlet.HttpHeader;
+import org.openecomp.sdc.logging.servlet.spring.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoggingInterceptorConfig {
+
+    @Value("${onap.logging.requestIdHeader}")
+    private String[] loggingRequestIdHeaders;
+
+    @Value("${onap.logging.partnerNameHeader}")
+    private String[] loggingPartnerNameHeader;
+
+    @Bean
+    public LoggingInterceptor createLoggingInterceptor() {
+        return new LoggingInterceptor(new HttpHeader(loggingRequestIdHeaders),
+                new HttpHeader(loggingPartnerNameHeader));
+    }
+}
