c8bdb5d92a4ce753c9fabd86a381048388ffaea9
avigaffa <avi.gaffa@amdocs.com>
2018-11-28 11:57:09 +0200

    Workflow Deprecation - Archive Workflow
    returning workflow archiving status in a field called "archiving" instead of "status" when listing workflows
    
    Change-Id: Ib75696257ecef0ef0646c0465e0bf6cb6d0533c1
    Issue-ID: SDC-1866
    Signed-off-by: avigaffa <avi.gaffa@amdocs.com>

--- workflow-designer-be/src/test/java/org/onap/sdc/workflow/TestUtil.java
+++ workflow-designer-be/src/test/java/org/onap/sdc/workflow/TestUtil.java
@@ -3,22 +3,25 @@ package org.onap.sdc.workflow;
 import static org.onap.sdc.workflow.services.impl.ItemType.WORKFLOW;
 
 import org.onap.sdc.workflow.services.types.Workflow;
+import org.onap.sdc.workflow.services.types.ArchivingStatus;
 import org.openecomp.sdc.versioning.types.Item;
+import org.openecomp.sdc.versioning.types.ItemStatus;
 
 public class TestUtil {
 
-    public static Workflow createWorkflow(int workflowNum, boolean createId) {
+    public static Workflow createWorkflow(int workflowNum, boolean createId, ArchivingStatus archivingStatus) {
         Workflow workflow = new Workflow();
         if (createId) {
             workflow.setId(String.valueOf(workflowNum));
         }
         workflow.setName("Workflow_" + workflowNum);
         workflow.setDescription("Description_" + workflowNum);
+        workflow.setArchiving(archivingStatus);
 
         return workflow;
     }
 
-    public static Item createItem(int itemNum, boolean setType, boolean setId) {
+    public static Item createItem(int itemNum, boolean setType, boolean setId, ItemStatus archivingStatus) {
         Item item = new Item();
         if (setId) {
             item.setId(String.valueOf(itemNum));
@@ -28,6 +31,7 @@ public class TestUtil {
         if (setType) {
             item.setType(WORKFLOW.name());
         }
+        item.setStatus(archivingStatus);
         return item;
     }
 
