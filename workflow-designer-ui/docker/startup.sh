48d36f7be55593a9c50006cd8dfe8cd7cb858a4a
r.bogacki <r.bogacki@samsung.com>
2019-10-10 08:48:03 +0200

    Enabled HTTPS for sdc-workflow-designer
    -Fixes for frontend and backend communication
    
    Change-Id: Ic8e27e1e8f116ccef23e032fbb02a99af56fa516
    Issue-ID: SDC-2479
    Signed-off-by: Robert Bogacki <r.bogacki@samsung.com>
    Signed-off-by: Krystian Kedron <k.kedron@partner.samsung.com>

--- workflow-designer-ui/docker/startup.sh
+++ workflow-designer-ui/docker/startup.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
+
 # adding support for https
 HTTPS_ENABLED=${IS_HTTPS:-"false"}
-
-if [ "$HTTPS_ENABLED" = "true" ]; then
+CLIENT_AUTH=${IS_CLIENT_AUTH:-"false"}
+if [ "$HTTPS_ENABLED" = "true" ]
+then
     echo "enable ssl"
-    if [ -n "$KEYSTORE_PATH" ]; then
 
-        keystore_pass="!ppJ.JvWn0hGh)oVF]([Kv)^"
-        truststore_pass="].][xgtze]hBhz*wy]}m#lf*"
+    java -jar "${JETTY_HOME}/start.jar" --add-to-start=https,ssl \
+        jetty.sslContext.keyStorePath=$KEYSTORE_PATH \
+        jetty.sslContext.keyStorePassword=$KEYSTORE_PASS \
+	    jetty.sslContext.keyManagerPassword=$KEYSTORE_PASS \
+        jetty.sslContext.trustStorePath=$TRUSTSTORE_PATH \
+        jetty.sslContext.trustStorePassword=$TRUSTSTORE_PASS
 
-        java -jar "${JETTY_HOME}/start.jar" --add-to-start=https,ssl \
-            jetty.sslContext.keyStorePath=$KEYSTORE_PATH \
-            jetty.sslContext.keyStorePassword=${KEYSTORE_PASS:-$keystore_pass} \
-            jetty.sslContext.trustStorePath=$TRUSTSTORE_PATH \
-            jetty.sslContext.trustStorePassword=${TRUSTSTORE_PASS:-$truststore_pass} 
-     else
-         echo "Using jetty default SSL"
-         java -jar "${JETTY_HOME}/start.jar" --add-to-start=https,ssl
-     fi
+    echo "setting SSL environment variable"
+
+    SSL_JAVA_OPTS=" -DkeystorePath=$JETTY_BASE/$KEYSTORE_PATH -DkeystorePassword=$KEYSTORE_PASS -DkeyManagerPassword=$KEYSTORE_PASS -DtruststorePath=$JETTY_BASE/$KEYSTORE_PATH -DtruststorePassword=$TRUSTSTORE_PASS -DsslTrustAll=$TRUST_ALL"
+
+    echo $SSL_JAVA_OPTS
+
 else
     echo "no ssl required"
 fi
-
-java -DproxyTo=$BACKEND $JAVA_OPTIONS -jar $JETTY_HOME/start.jar
+java $JAVA_OPTIONS -DproxyTo=$BACKEND $SSL_JAVA_OPTS -jar $JETTY_HOME/start.jar
 
