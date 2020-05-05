677981bc5745a3bdd8e7991dddb970e732e1a576
andre.schmid <andre.schmid@est.tech>
2020-05-05 11:11:54 +0100

    Adjust docker-compose and SSL config
    SSL is disabled by default as it is documented in the README.
    Expose a port for cassandra docker.
    Fix sdc-workflow-backend SDC_ENDPOINT environment variable.
    Add some build artifacts to gitignore.
    
    Issue-ID: SDC-3019
    Change-Id: If4d81752f6b7c2c1ec2a73327bebbb8ef3ce0420
    Signed-off-by: andre.schmid <andre.schmid@est.tech>

--- sdc-workflow-designer-be/docker/startup.sh
+++ sdc-workflow-designer-be/docker/startup.sh
@@ -3,6 +3,7 @@
 HTTPS_ENABLED=${SERVER_SSL_ENABLED:-"false"}
 if [ "$HTTPS_ENABLED" = "true" ]
 then
+    echo "HTTPS is enabled. Configuring keystore."
     KEYSTORE=${SERVER_SSL_KEYSTORE_PATH}
     if [ -f "$KEYSTORE" ]; then
         echo "$KEYSTORE exist"
@@ -24,5 +25,7 @@ then
         cp /truststore $TRUSTSTORE_DIR
         chmod 755 $TRUSTSTORE
     fi
+else
+  echo "HTTPS is disabled."
 fi
 java ${JAVA_OPTIONS} -jar /app.jar ${SPRING_BOOT_OPTIONS}
