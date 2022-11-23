60aafff750f9d596f9a382b435f1309eb8448beb
vasraz <vasyl.razinkov@est.tech>
2022-11-23 11:09:05 +0000

    Remove 'Powered by Jetty' from default jetty response
    Signed-off-by: Vasyl Razinkov <vasyl.razinkov@est.tech>
    Change-Id: Ic9d5e8bc8dd2b1fc9983158d5871c3317aae1333
    Issue-ID: SDC-4277

--- sdc-workflow-designer-ui/docker/startup.sh
+++ sdc-workflow-designer-ui/docker/startup.sh
@@ -3,25 +3,26 @@
 # adding support for https
 HTTPS_ENABLED=${IS_HTTPS:-"false"}
 CLIENT_AUTH=${IS_CLIENT_AUTH:-"false"}
-if [ "$HTTPS_ENABLED" = "true" ]
-then
-    echo "enable ssl"
+if [ "$HTTPS_ENABLED" = "true" ]; then
+  echo "enable ssl"
 
-    java -jar "${JETTY_HOME}/start.jar" --add-to-start=https,ssl \
-        jetty.sslContext.keyStorePath=$KEYSTORE_PATH \
-        jetty.sslContext.keyStorePassword=$KEYSTORE_PASS \
-	    jetty.sslContext.keyManagerPassword=$KEYSTORE_PASS \
-        jetty.sslContext.trustStorePath=$TRUSTSTORE_PATH \
-        jetty.sslContext.trustStorePassword=$TRUSTSTORE_PASS
+  java -jar "${JETTY_HOME}/start.jar" --add-to-start=https,ssl \
+    jetty.sslContext.keyStorePath=$KEYSTORE_PATH \
+    jetty.sslContext.keyStorePassword=$KEYSTORE_PASS \
+    jetty.sslContext.keyManagerPassword=$KEYSTORE_PASS \
+    jetty.sslContext.trustStorePath=$TRUSTSTORE_PATH \
+    jetty.sslContext.trustStorePassword=$TRUSTSTORE_PASS
 
-    echo "setting SSL environment variable"
+  echo "setting SSL environment variable"
 
-    SSL_JAVA_OPTS=" -DkeystorePath=$JETTY_BASE/$KEYSTORE_PATH -DkeystorePassword=$KEYSTORE_PASS -DkeyManagerPassword=$KEYSTORE_PASS -DtruststorePath=$JETTY_BASE/$KEYSTORE_PATH -DtruststorePassword=$TRUSTSTORE_PASS -DsslTrustAll=$TRUST_ALL"
+  SSL_JAVA_OPTS=" -DkeystorePath=$JETTY_BASE/$KEYSTORE_PATH -DkeystorePassword=$KEYSTORE_PASS -DkeyManagerPassword=$KEYSTORE_PASS -DtruststorePath=$JETTY_BASE/$KEYSTORE_PATH -DtruststorePassword=$TRUSTSTORE_PASS -DsslTrustAll=$TRUST_ALL"
 
-    echo $SSL_JAVA_OPTS
+  echo $SSL_JAVA_OPTS
 
 else
-    echo "no ssl required"
+  echo "no ssl required"
 fi
-java $JAVA_OPTIONS -DproxyTo=$BACKEND $SSL_JAVA_OPTS -jar $JETTY_HOME/start.jar
 
+echo "jetty.httpConfig.sendServerVersion=false" >> ${JETTY_BASE}/start.d/start.ini
+
+java ${JAVA_OPTIONS} -DproxyTo=${BACKEND} ${SSL_JAVA_OPTS} -jar ${JETTY_HOME}/start.jar
